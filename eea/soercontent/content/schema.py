39b10e2714f420071de54dd759bb2c3923c64f8c
Alin Voinea <alin@eaudeweb.ro>
2014-03-11 19:39:27 +0200

    Refs #18825 - Add long summary and notes fields to Fiche schema

--- eea/soercontent/content/schema.py
+++ eea/soercontent/content/schema.py
@@ -10,6 +10,25 @@ from eea.soercontent.content.interfaces import ISoerContent
 
 
 SCHEMA = atapi.Schema((
+    atapi.TextField("summary",
+          schemata="default",
+          required=False,
+          default_output_type="text/x-html-safe",
+          widget=atapi.RichWidget(
+              label=_(u'Long summary'),
+              description=_(u'Detailed summary or key messages'),
+              rows=25)
+    ),
+    atapi.TextField("notes",
+          schemata="default",
+          required=False,
+          default_output_type="text/x-html-safe",
+          widget=atapi.RichWidget(
+              label=_(u'References and endnotes'),
+              description=_(u'References, endnotes, footnotes, '
+                            u'abbreviations and definitions'),
+              rows=25)
+    ),
     atapi.ImageField(
         name="image",
         schemata="default",
@@ -29,8 +48,8 @@ SCHEMA = atapi.Schema((
         )
     ),
     atapi.BooleanField(
-        schemata='settings',
         name='presentation',
+        schemata='settings',
         required=False,
         languageIndependent=True,
         widget=atapi.BooleanWidget(
@@ -38,7 +57,7 @@ SCHEMA = atapi.Schema((
             description=_(u"If selected, this will give users the ability to "
                           u"view the contents as presentation slides.")
         )
-    )
+    ),
 ))
 
 class SoerContent(folder.ATFolder, document.ATDocumentBase):
@@ -54,5 +73,12 @@ class SoerContent(folder.ATFolder, document.ATDocumentBase):
         document.ATDocumentSchema.copy() +
         SCHEMA.copy()
     )
+
+    schema['description'].widget.label = _(u"Short summary")
+
+    schema.moveField('summary', after="description")
+
     schema['text'].widget.helper_js = ("eeacharlimit_enabled.js",)
     schema['text'].widget.helper_css = ("eeacharlimit.css",)
+
+    schema.moveField('notes', after='text')
