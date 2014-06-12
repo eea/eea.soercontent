cbe652faa9d904555859b923422e77e004d20934
ichim-david <ichim.david@gmail.com>
2014-06-12 17:50:43 +0300

    Refs #20024; started adding the logic for retriving the folder contents dynamically

--- eea/soercontent/browser/app/view.py
+++ eea/soercontent/browser/app/view.py
@@ -19,3 +19,43 @@ class FicheView(BrowserView):
 
     def __call__(self, **kwargs):
         return self.index()
+
+
+class SOERContentView(BrowserView):
+    """ Content page
+    """
+
+    def __init__(self, context, request):
+        super(SOERContentView, self).__init__(context, request)
+
+    def get_parent(self, parent_id=None):
+        """
+        :param parent_id:
+        :return: :rtype:
+        """
+        return self.context.portal_catalog(id=parent_id)
+
+    def content(self, parent_id=None, object_id=None):
+        """
+        :param parent_id:
+        :param object_id:
+        :return:
+        :rtype:
+        """
+        parent = self.get_parent(parent_id)
+        if not parent:
+            return
+        obj = parent[0].getObject().restrictedTraverse(object_id)
+        children = self.context.portal_catalog(
+            {'path': '/'.join(obj.getPhysicalPath()), 'depth': 1},
+            portal_type='Fiche')
+        res = []
+        for brain in children:
+            res.append({
+                'url': brain.getURL(),
+                'title': brain.Title
+            })
+        return res
+
+
+
