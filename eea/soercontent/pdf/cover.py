2be9b5509fdc3bc5436c7b610287216e9728def6
ichim-david <ichim.david@gmail.com>
2015-01-30 17:55:55 +0200

    Refs #22442; Changed the following:
    - Use the images of the first 3 Briefings if the Folder doesn't
      have any cover images instead of the themes since the Briefings
      inside the Folder can belong to many themes

--- eea/soercontent/pdf/cover.py
+++ eea/soercontent/pdf/cover.py
@@ -10,6 +10,24 @@ class Cover(PDFCover):
     """
     template = ViewPageTemplateFile('soer2015_cover.pt')
 
+    def has_cover_image(self):
+        """
+        :return:
+        :rtype:
+        """
+        return self.context.coverImage.width
+
+    def get_children_cover_images(self):
+        """
+        :return:
+        :rtype:
+        """
+        briefings = self.context.getFolderContents(
+            {"portal_type": "Fiche", "sort_limit": 3,
+             "sort_on": "getObjPositionInParent"})
+        briefings = [b.getObject() for b in briefings]
+        return briefings
+
 
 class BackCover(PDFBackCover):
     """ PDF Back cover
