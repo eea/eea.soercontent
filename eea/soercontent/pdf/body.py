de0b74809cc4c863053f8db2c7e3480ba6c94f39
ichim-david <ichim.david@gmail.com>
2015-02-23 14:47:26 +0200

    * Bugfix: display content for collections not just folders and have the cover   background color be the default EEA Blue   [ichimdav refs #23305]

--- eea/soercontent/pdf/body.py
+++ eea/soercontent/pdf/body.py
@@ -1,10 +1,16 @@
 """ PDF View
 """
 from logging import getLogger
+from Acquisition import aq_base
+
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from eea.pdf.themes.page.body import Body as PDFBody
 from eea.pdf.themes.book.folder import Body as FolderBody
+
+
 logger = getLogger('eea.soercontent')
+from Products.CMFPlone.utils import safe_hasattr
 
 
 class Body(PDFBody):
@@ -21,3 +27,13 @@ class Soer2015Body(FolderBody):
     """ Soer2015 folder custom body
     """
     template = ViewPageTemplateFile('soer2015_body.pt')
+
+    @property
+    def brains(self):
+        """ Brains of collections or folderish content
+        """
+        if safe_hasattr(aq_base(self.context), 'queryCatalog'):
+            return self.context.queryCatalog(batch=False)[:self.maxbreadth]
+        else:
+            return self.context.getFolderContents()[:self.maxbreadth]
+
