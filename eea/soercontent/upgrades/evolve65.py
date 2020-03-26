0cab100d1bc905a903e2e8dd7921e963f3151940
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-03-26 13:33:40 +0100

    Refs #111954 Fix versioning in upgrade step after moving items.

--- eea/soercontent/upgrades/evolve65.py
+++ eea/soercontent/upgrades/evolve65.py
@@ -2,6 +2,7 @@
 """
 import logging
 import transaction
+from eea.versions.interfaces import IVersionControl
 from plone.api import content
 from Products.CMFCore.utils import getToolByName
 logger = logging.getLogger('eea.soercontent')
@@ -16,10 +17,51 @@ def move_soer_content(context):
     soer_2020 = context.restrictedTraverse("/www/SITE/soer-2020")
     soer = context.restrictedTraverse("/www/SITE/soer") 
 
+    paths = ["/www/SITE/soer-2015", "/www/SITE/soer-2020"]
+    vers_dict = {}
+    for path in paths:
+        brains = ctool.searchResults(path=path, Language="all")
+
+        for brain in brains:
+            obj = brain.getObject()
+            try:
+                control = IVersionControl(obj)
+            except:
+                continue
+            version_id = control.getVersionId() or None
+
+            if obj.id in ['soer-2015', 'soer-2020'] and obj.portal_type == 'Folder':
+                logger.info('UPDATING FOLDER %s' % obj.absolute_url())
+                vers_dict.update({obj.id.split('-')[-1]: version_id})
+            else:
+                vers_dict.update({obj.id: version_id})
+
     new_soer_2015 = content.move(soer_2015, soer, '2015')
     new_soer_2020 = content.move(soer_2020, soer, '2020')
     default_page = context.restrictedTraverse("/www/SITE/soer/2020/soer-2020")
     default_page = content.move(default_page, soer, 'soer-2020')
+
+    paths = ["/www/SITE/soer/2015", "/www/SITE/soer/2020", "/www/SITE/soer/soer-2020"]
+    for path in paths:
+        brains = ctool.searchResults(path=path, Language="all")
+
+        for brain in brains:
+            obj = brain.getObject()
+            try:
+                control = IVersionControl(obj)
+            except:
+                continue
+
+            version_id = control.getVersionId() or None
+            if vers_dict.get(obj.id, None):
+                if vers_dict.get(obj.id, None) == version_id:
+                    logger.info('GOOD OBJECT %s' % obj.absolute_url())
+                else:
+                    logger.info('BAD OBJECT %s' % obj.absolute_url())
+                    control.setVersionId(vers_dict.get(obj.id))
+                    boj._p_changed = True
+                    obj.reindexObject()
+                    transaction.commit()
 
     logger.info('Finished moving soer content ... DONE')
     return 'Done moving'
