50f9a5a863bfa2c2ca427caf11e71c16a31e1ce8
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-04-27 19:44:47 +0200

    Refs #111954 Soer content tweaks for 2010 folder.

--- eea/soercontent/upgrades/evolve68.py
+++ eea/soercontent/upgrades/evolve68.py
@@ -2,13 +2,10 @@
 """
 import logging
 import transaction
+from eea.geotags.interfaces import IGeoTags
 from plone.api import content
+from zope.component import getAdapter
 from Products.CMFCore.utils import getToolByName
-from zope.component import queryUtility
-from Products.ATContentTypes.interface import IATCTTool
-from eea.versions.interfaces import IVersionControl
-from eea.versions.versions import assign_version
-
 
 logger = logging.getLogger('eea.soercontent')
 
@@ -32,30 +29,39 @@ def tweaks(context):
         if key in folder_keys:
             setattr(soer_2010, key, value)
 
-    atool = queryUtility(IATCTTool)
-    metadata = atool.getAllMetadata(True)
-    brain_2010 = catalog.searchResults(path={'query': '/www/SITE/soer/2010', 'depth': 0})[0]
-    brain_page_2010 = catalog.searchResults(path={'query': '/www/SITE/soer/2010/2010', 'depth': 0})[0]
+    soer_2010.setTitle(soer_page_2010.Title())
+    soer_2010.setDescription(soer_page_2010.Description())
+    soer_2010.setEffectiveDate(soer_page_2010.getEffectiveDate())
+    soer_2010.setCreationDate(soer_page_2010.creation_date)
+    soer_2010.setSubject(soer_page_2010.Subject())
+    soer_2010.setCreators(soer_page_2010.Creators())
+    soer_2010.changeOwnership(soer_page_2010.getOwner())
+    soer_2010.temporalCoverage = soer_page_2010.temporalCoverage
 
-    metadata += ['geotags', 'effective', 'created']
-    for key in metadata:
-        try:
-            value = getattr(brain_page_2010, key)
-            setattr(brain_2010, key, value)
-        except:
-            continue
+    # Map relatedItems
+    forwards = soer_page_2010.getRelatedItems()
+    backs = soer_page_2010.getBackReferences()
 
-    soer_2010.effective_date = soer_page_2010.effective_date
-    # control = IVersionControl(soer_page_2010)
-    # version_id = control.getVersionId() or None
-    # assign_version(soer_2010, version_id)
+    if forwards:
+        soer_2010.setRelatedItems(forwards)
+
+    for ob in backs:
+        related = ob.getRelatedItems()
+        related.append(soer_2010)
+        ob.setRelatedItems(related)
+
+    # set geotags
+    geo_page_2010 = getAdapter(soer_page_2010, IGeoTags)
+    geo_2010 = getAdapter(soer_2010, IGeoTags)
+    geo_2010._set_tags(geo_page_2010.tags)
 
     soer_2010._p_changed = True
     soer_2010.reindexObject()
-    # catalog.unindexObject(soer_2010)
     catalog.reindexObject(soer_2010, update_metadata=True)
     transaction.commit()
+
     soer = context.restrictedTraverse("/www/SITE/soer")
     content.transition(obj=soer, transition='publish')
+
     logger.info('Finished tweaking moved soer content ... DONE')
     return 'Done moving'
